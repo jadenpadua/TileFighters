89d600e4eb43a48bc161a5fdc903d6a7a5439783
Jaden Padua <paduajaden@gmail.com>
2019-12-20 19:15:42 -0800

    Create basic grid and sprite movement

---
+++ sprites.py
@@ -0,0 +1,37 @@
+
+import pygame as pg
+from settings import *
+# Player class making yellow rectangle sprite
+class Player(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+    # Allows sprite to move, setting x and y positions
+    def move(self, dx=0, dy=0):
+        self.x += dx
+        self.y += dy
+    # Updates sprite's position
+    def update(self):
+        self.rect.x = self.x * TILESIZE
+        self.rect.y = self.y * TILESIZE
+# Wall object 
+class Wall(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+    
+
